79717c46d18011da7161ed0d23c38a8aaf28f380
azammunawar <azi.munawarali@gmail.com>
2021-08-04 22:06:11 +0500

    Basic boilerplate added

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: path.join(__dirname, "src", "index.js"),
+    output: {
+        path: path.resolve(__dirname, "dist"),
+    },
+    devServer: { port: "9001" },
+    module: {
+        rules: [
+            {
+                test: /\.?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        // either define it here or in .babelrc 
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "index.html"),
+        }),
+    ],
+}
