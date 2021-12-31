c06de76978374632aab244d8cf6414f49ac0fcd5
azammunawar <azi.munawarali@gmail.com>
2021-12-31 19:38:47 +0500

    about page added

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -2,6 +2,6 @@ import React, { Component, useContext } from "react";
 import { StoreContext } from "store";
 
 const About = () => {
-  return <div>about </div>;
+  return <div>about page added </div>;
 };
 export default About;
