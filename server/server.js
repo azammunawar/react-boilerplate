ca46c3f6a7814b93f7cb15a1739319a9a51cc26a
azammunawar <azi.munawarali@gmail.com>
2022-02-04 03:04:54 +0500

    server

--- server/server.js
+++ server/server.js
@@ -68,7 +68,7 @@ app.use("*", (req, res) => {
   });
 });
 
-app.listen(80, () => {
+app.listen(process.env.PORT || 3000, () => {
   // eslint-disable-next-line no-console
   console.log("runing on 3001");
 });
