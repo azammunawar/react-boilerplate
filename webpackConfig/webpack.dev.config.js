cded682ff36099434c95bd9db7c3166d472fc747
azammunawar <azi.munawarali@gmail.com>
2023-01-30 00:46:40 +0500

    filename fixed

--- webpackConfig/webpack.dev.config.js
+++ webpackConfig/webpack.dev.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const { merge } = require("webpack-merge");
-const common = require("./webpack.common.config.js/index.js.js");
+const common = require("./webpack.common.config");
 // const { EnvironmentPlugin } = require("webpack");
 
 module.exports = merge(common, {
